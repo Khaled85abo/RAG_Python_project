8958660d0fa734ee242dd89186bb797d718113ae
Khaled85abo <khaledabou1985@gmail.com>
2024-05-26 22:20:19 +0200

    Add ollama server function

---
+++ llama3_lm.py
@@ -0,0 +1,26 @@
+import requests
+import json
+URL = "http://localhost:11434/api/generate"
+data = {
+	"model": "llama3:8b",
+	"stream": False,
+	"prompt": "tell me a joke"
+}
+
+
+def llama3_lm(prompt: str):
+    data["prompt"] = prompt
+    try:
+        print(f"Data: {data}")
+        res = requests.post(url=URL, data=json.dumps(data), headers={"Content-Type": "application/json"})
+        res = res.json()
+        return res["response"]
+    except Exception as e:
+        print(f"Error getting response from llama3: {e}")
+
+
+if __name__ == "__main__":
+    prompt = input("prompt: ")
+    answer = llama3_lm(prompt=prompt)
+    print(answer)
+    print("---------------------")
